950c342aa5964677ad1e9f282d9a38e8b0e33620
Mehul-vaishnav01 <vaishnavmehul12@gmail.com>
2026-08-22 01:44:08 +0530

    Sender Balance Calculation Implemented

--- Backend/src/controller/transaction.controller.js
+++ Backend/src/controller/transaction.controller.js
@@ -56,28 +56,51 @@ const emailService=require('../services/email.service')
         {
             if(isTransactionAlreadyExist.status==='Completed')
             {
-                res.status(200).json({
+                return res.status(200).json({
                     message:"Transaction already processed",
                     transaction:isTransactionAlreadyExist
                 })
             }
             if(isTransactionAlreadyExist.status==='Pending')
             {
-                res.status(200).json({
+                return res.status(200).json({
                     message:"Transaction already processing"
                 })
             }
             if(isTransactionAlreadyExist.status==='Failed')
             {
-                res.status(500).json({
+                return res.status(500).json({
                     message:"Transaction processing failed , plese retry"
                 })
             }
             if(isTransactionAlreadyExist.status==='Reversed')
             {
-                res.status(500).json({
+                return res.status(500).json({
                     message:"Transaction processing reversed , plese retry"
                 })
             }
+        }
+
+        /**
+         * 3.Check Account Status
+         */
+
+        if(fromUserAccount.status!=="ACTIVE"||toUserAccount!=="ACTIVE")
+        {
+            return res.status(400).json({
+                message:"Both fromAccount & toAccount must be ACTIVE to process transaction"
+            })
+        }
+
+        /**
+         * 4.Derive Sender Balance From Ledger
+         */
+
+        const balance=await fromAccount.getBlance()
+        if(balance<amount)
+        {
+            return res.status(400).json({
+                message:`Insufficient balance. Current Balance is ${balance}. Requested amount is ${amount}.`
+            })
         }
     }
--- Backend/src/models/account.model.js
+++ Backend/src/models/account.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const ledgerModel=require('./ledger.model');
+const { $where } = require('./user.model');
 const accountSchema=new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
@@ -25,6 +26,48 @@ const accountSchema=new mongoose.Schema({
 })
 
 accountSchema.index({user:1,status:1})
+
+accountSchema.methods.getBlance=async function () {
+    const balanceData=await ledgerModel.aggregate([
+        {$match:{account:this._id}},
+        {
+            $group:{
+                _id:null,
+                totalDebit:{
+                    $sum:{
+                        $cond:[
+                            {$seq:["$type","DEBIT"]},
+                            "amount",
+                            0
+                        ]
+                    }
+                },
+                totalCredit:{
+                    $sum:{
+                        $cond:[
+                            {$seq:["$type","CREDIT"]},
+                            "amount",
+                            0
+                        ]
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                _id=0,
+                balance:{$subtract:["totalCredit","totalDebit"]}
+            }
+        }
+    ])
+
+    if(balanceData.length===0)
+    {
+        return 0;
+    }
+
+    return balanceData[0].balance
+}
 
 const accountModel=mongoose.model("account",accountSchema)
 
